cc7b2f2ec99ea9d3845256d757c7ff74d9681477
Michael Rice <michael@riceclan.org>
2014-12-14 09:27:07 -0600

    force setuptools 7.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,17 +15,25 @@
 # limitations under the License.
 # 
 
-echo -n "Looking for bin directory... "
+cat << EOF
+************
+Bootstrapping buildout.  This will create buildout.cfg and bootstrap.py
+in the current directory
+   `pwd`
 
-if [ "${PWD%/bin}" != "${PWD}" ]; then
-    cd "${PWD%/bin}"
+************
+If this is NOT what you want, hit ^C now
+EOF
+echo -n "Ok to continue? "
+read ans
+
+BBSURL="http://downloads.buildout.org/2/bootstrap.py"
+BBSURL="https://raw.githubusercontent.com/buildout/buildout/master/bootstrap/bootstrap.py"
+
+if [ ! -f bootstrap.py ]; then
+
+    curl -q -z bootstrap.py -O $BBSURL
 fi
-
-if [ ! -d "bin" ]; then
-    mkdir bin
-fi
-
-(cd bin && curl -q -z bootstrap.py -O http://downloads.buildout.org/2/bootstrap.py )
 
 if [ ! -f buildout.cfg ]; then
 
@@ -52,17 +60,23 @@ recipe = zc.recipe.egg
 eggs = $egg
 
 EOT
+
+  cat >> buildout.cfg <<EOT
+[versions]
+setuptools = 7.0
+
+EOT
   
   done
   
 fi
 
 # First pass, get virtualenv installed so that we can use the local python
-python bin/bootstrap.py
-bin/buildout
-bin/virtualenv .
-source bin/activate
+python bootstrap.py --setuptools-version 7.0 || exit   # configure for bin/buildout
+bin/buildout || exit          # run buildout to get egss from buildout.cfg
+bin/virtualenv . || exit      # configure to use virtualenv
+source bin/activate || exit   # activate via virtualenv
 
 # Second pass, rebuild using the local python
-python bin/bootstrap.py
-bin/buildout
+python bootstrap.py --setuptools-version 7.0 || exit   # re-run bootstrap with the local python
+bin/buildout || exit          # re-run buildout
