2060bff495f091cd686b5b38c7b8bb21f18cb6ac
Michael Rice <michael@riceclan.org>
2014-12-25 12:07:16 -0600

    refactored bootstrap.sh
    1) allow for building a local python instead of using one already on the OS
       (in case there isn't one already, or in case you want to play with newer
       versions).
    
    2) allowed for a --prefix for installing python, and a --workdir for installing
       your virtualenv workspaces
    
    3) added virtualenvwrapper to the default/template buildout.cfg
    
    4) made everything a commandline parameter.  Looked at and discarded using
       argument parsers including getopts, getopt, getopt_long, and getopts_long.
       I only wanted long options and it was easiest (this pass) to write my own.
    
    5) removed the prompt for asking for extra modules to add to buildout.cfg.
       Adding these manually is a (relatively simple) exercise for the reader.
    
    5) tried to do some sanity checking on the parameters given.  There are still
       some nasty bugs lurking, so I may work on these more in the near future.
    
    6) started to work on passing in proxy settings so that the bootstrap.sh
       script can control that for wget/curl.  Unfortunately I have a hostile
       proxy at work and I need some more testing to try to find the combination
       that works for all of wget, curl, and python (buildout, pip, etc).

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,82 +1,240 @@
 #!/bin/bash
 
-# buidout-bootstrap - how to bootstrap python buildout with virtualenv
-# Copyright (C) 2014 Michael Rice <michael at riceclan dot org>
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-# http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# 
-
-cat << EOF
-************
-Bootstrapping buildout.  This will create buildout.cfg and bootstrap.py
-in the current directory
-   `pwd`
-
-************
-If this is NOT what you want, hit ^C now
-EOF
-echo -n "Ok to continue? "
-read ans
-
+# The buildout bootstrap url to use
+GITPIPURL="https://raw.github.com/pypa/pip/master/contrib/get-pip.py"
 BBSURL="http://downloads.buildout.org/2/bootstrap.py"
 BBSURL="https://raw.githubusercontent.com/buildout/buildout/master/bootstrap/bootstrap.py"
 
-if [ ! -f bootstrap.py ]; then
+# Process Commandline Arguments ##>
+# bootstrap.py
+# --use /path/to/python
+# --prefix /some/where/to/work
+# --workdir /some/where/else
+# or
+#
+# bootstrap.py
+# --build /path/to/python.tgz
+# --prefix /some/where/to/work
+# --workdir /some/where/else
 
-    curl -q -z bootstrap.py -O $BBSURL
+usepython=""
+useprefix=""
+useworkdir=""
+usebuild=""
+
+while [ "$#" -ge 1  ]; do
+    case "$1" in
+        --use) usepython="$2"; shift;;
+        --use=*) usepython="${1#--use=}";;
+        --prefix) useprefix="$2"; shift;;
+        --prefix=*) useprefix="${1#--prefix=}";;
+        --workdir) useworkdir="$2"; shift;;
+        --workdir=*) useworkdir="${1#--workdir=}";;
+        --build) usebuild="$2"; shift;;
+        --build=*) usebuild="${1#--usebuild=}";;
+        --proxy) useproxy="$2"; shift;;
+        --proxy=*) useproxy="${1#--useproxy=}";noproxy=false;;
+        --no-proxy) useproxy=""; noproxy=true;;
+        --wget-options) wgetoptions="$2"; shift;;
+        --wget-options=*) wgetoptions="${1#--wget-options=}"; shift;;
+        --curl-options) curloptions="$2"; shift;;
+        --curl-options=*) curloptions="${1#--curl-options=}"; shift;;
+    esac
+    shift
+done ##<
+
+
+##> Define some variable defaults
+usepython_valid=false
+useprefix_valid=false
+useworkdir_valid=false
+usebuild_valid=false
+
+##<
+inspect_tarball() { ##>
+    _tarball="$1"
+    case "$_tarball" in
+       *.tar.gz|*.tgz)  _zopt="-z";;
+       *.tar.bz2|*.tbz2) _zopt="-j";;
+       *.tar.xz|*.txz) _zopt="-J";;
+       *.tar.Z) _zopt="-Z";;
+       *) _zopt="";
+    esac
+
+    _directory="`tar $_zopt -tf $_tarball | head -1`"
+} ##<
+
+die() { ##>
+    format=$1
+    case "$format" in
+        *"\n") :;;
+        *) format="$format\n";;
+    esac
+    shift
+    printf "$format" "$@" >&2
+    exit 1
+} ##<
+
+# cannot specify both a python already installed and to build our own
+if [ -n "$usepython" -a -n "$usebuild" ]; then
+    die "Error, --use is invalid with --build and vice versa" 
 fi
 
-if [ ! -f buildout.cfg ]; then
+# but must specify one of use python or build python
+if [ -z "$usepython" -a -z "$usebuild" ]; then
+    die "Error: one of --use or --build is required"
+fi
 
-  eggs="pip virtualenv"
-  
-  echo "Automatic: setuptools, pip, virtualenv, buildout"
-  echo "Enter additional eggs>> "
-  read addeggs
-  
-  eggs="$eggs $addeggs"
+# if we're building, we require a prefix directory
+if [ -n "$usebuild" -a -z "$useprefix" ]; then
+    eval usebuild="$usebuild"     # expand specials
+    die "Error: --build requires --prefix"
+fi
 
-  cat > buildout.cfg <<EOT
+# validate that usepython is an executable (if it is executable and not
+# python it won't work either).
+if [ -n "$usepython" -a -f "$usepython" -a -x "$usepython" ]; then
+    eval usepython="$usepython"   # expand specials
+    usepython_valid=true
+fi
+
+# validate that the workdir specified either already exists as a directory
+# or that it is creatable. 
+if test -d "$useworkdir" || mkdir -p "$useworkdir" >/dev/null; then
+    eval useworkdir="$useworkdir"   # expand specials
+    useworkdir_valid=true
+fi
+
+if ! "$useworkdir_valid"; then
+    die "Cannot use workdir \"$useworkdir\""
+fi
+
+# validate that the prefix specified either
+#    exists, is a directory, and is empty
+#    or
+#    can be created
+if [ -n "$useprefix" ]; then
+    eval useprefix="$useprefix"   # expand specials
+    if [ -d "$useprefix" ]; then
+        if [ -n "`ls -A $useprefix`" ]; then
+            die "Error: prefix \"%s\" is not empty!" "$useprefix"
+        else
+            useprefix_valid=true
+        fi
+    elif mkdir -p "$useprefix" >/dev/null; then
+        useprefix_valid=true
+    fi
+fi
+
+if ! "$useprefix_valid"; then
+    die "Cannot work in $useprefix."
+fi
+
+
+# find one of wget or curl in the path
+usewget=false
+usecurl=false
+
+if `which curl > /dev/null`; then
+    usecurl=true
+elif `which wget > /dev/null`; then
+    usewget=true
+fi
+
+download() { ##> uses wget or curl and any proxy settings to download a URL
+    _url="$1"
+
+    if ! "$noproxy" && test -n "$useproxy"; then
+        export HTTP_PROXY="$useproxy"
+        export HTTPS_PROXY="$useproxy"
+        export http_proxy="$useproxy"
+        export https_proxy="$useproxy"
+    fi
+    if $usecurl; then
+        curl -q -O "$_url"
+    elif $usewget; then
+        wget -q -O - "$_url"
+    fi
+} ##<
+
+create_buildout_config() { ##> create our buildout.cfg template
+cat <<EOF > buildout.cfg
+# this is a skeleton buildout.cfg to install pip, virtualenv, and
+# virtualenvwrapper.  You can read about zc.buildout and it's config
+# file format once you've installed zc.buildout, and configure your
+# own list of modules for buildout to manage.  
+#
+# Note that in this scheme pip, setuptools, setup.py, and buildout
+# are all configured to do virtually the same things.  buildout will
+# uninstall modules installed in those other ways, so be sure to
+# update this config if you intend to use buildout.
+#
 [buildout]
-parts = $eggs
+parts = pip virtualenv virtualenvwrapper
 bin-directory = bin
 
-EOT
-  
-  for egg in ${eggs}; do
-  
-  cat >> buildout.cfg <<EOT
-[$egg]
+[pip]
 recipe = zc.recipe.egg
-eggs = $egg
+eggs = pip
 
-EOT
+[virtualenv]
+recipe = zc.recipe.egg
+eggs = virtualenv
 
-  cat >> buildout.cfg <<EOT
-[versions]
-setuptools = 7.0
+[virtualenvwrapper]
+recipe = zc.recipe.egg
+eggs = virtualenvwrapper
 
-EOT
-  
-  done
-  
+EOF
+} ##<
+
+if ! "$usecurl" && ! "$usewget"; then
+    die "Couldn't find wget or curl, aborting, have to download stuff"
 fi
 
-# First pass, get virtualenv installed so that we can use the local python
-python bootstrap.py --setuptools-version 7.0 || exit   # configure for bin/buildout
-bin/buildout || exit          # run buildout to get egss from buildout.cfg
-bin/virtualenv . || exit      # configure to use virtualenv
-source bin/activate || exit   # activate via virtualenv
+if [ -n "$usebuild" -a -e "$usebuild" -a -f "$usebuild" ]; then
+    case "$usebuild" in
+        *.tar.gz|*.tar|*.tgz|*.tar.bz2|*.tbz2|*.tar.xz|*.txz)
+            inspect_tarball "$usebuild"
+            if [ "$_directory" ]; then
+                usebuild_valid=true
+            fi
+    esac
+fi
 
-# Second pass, rebuild using the local python
-python bootstrap.py --setuptools-version 7.0 || exit   # re-run bootstrap with the local python
-bin/buildout || exit          # re-run buildout
+if "$usebuild_valid" && "$useprefix_valid"; then
+    mkdir -p "$useprefix/.build"
+    cd "$useprefix/.build"
+    tar $_zopt -xf $usebuild
+    cd $_directory
+    echo "Configuring Python..."
+    ./configure --prefix=$useprefix > configure.log 2>&1
+    echo "Building Python..."
+    make > make.log 2>&1
+    echo "Installing Python..."
+    make install > install.log 2>&1
+    usepython="$useprefix/bin/python"
+    usepython_valid=true
+fi
+
+if "$useworkdir_valid"; then
+    mkdir -p $useworkdir
+else
+    die "Workdir is not valid"
+fi
+
+if "$usepython_valid" && "$useworkdir_valid"; then
+    mkdir -p "$useworkdir"
+    cd "$useworkdir"
+    download "$BBSURL"
+    create_buildout_config
+    $usepython bootstrap.py
+    bin/bootstrap
+    bin/virtualenv .
+    source bin/activate
+    bin/python bootstrap.py
+    bin/bootstrap
+else
+    die "Error, got to bootstrap without a valid python or workdir"
+fi
+
